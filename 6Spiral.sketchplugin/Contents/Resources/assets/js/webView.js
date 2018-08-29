1850e7c0190f6472e017d4aff15c941966db0f8d
invisiblegorilla <matej@outofgalaxy.com>
2018-08-29 13:22:47 -0700

    initial commit.

---
+++ 6Spiral.sketchplugin/Contents/Resources/assets/js/webView.js
@@ -0,0 +1,68 @@
+
+// Commented out so that it's possible to Right Click -> Inspect Element.
+// Disable the context menu
+// document.addEventListener("contextmenu", function(e) {
+//   e.preventDefault();
+// });
+
+var innerR = document.getElementById('InnerRadius');
+var outerR = document.getElementById('OuterRadius');
+var revolutions = document.getElementById('Revolutions');
+var degrees = document.getElementById('Degrees');
+var points = document.getElementById('Points');
+var degreeIncrementLabel = document.getElementById('DegreeIncrementLabel');
+var spiralGapLabel = document.getElementById('SpiralGapLabel');
+
+innerR.addEventListener('input', function (evt) {
+  setSpiralGapLabel();
+});
+
+outerR.addEventListener('input', function (evt) {
+  setSpiralGapLabel();
+});
+
+revolutions.addEventListener('input', function (evt) {
+  console.log(this.value);
+  degrees.value = this.value*360;
+  setDegreeIncrementLabel();
+  setSpiralGapLabel();
+});
+
+degrees.addEventListener('input', function (evt) {
+  console.log(this.value);
+  revolutions.value = this.value/360;
+  setDegreeIncrementLabel();
+});
+
+points.addEventListener('input', function (evt) {
+  console.log(this.value);
+  setDegreeIncrementLabel();
+});
+
+function setDegreeIncrementLabel() {
+  var degreeIncrement = degrees.value/(Math.floor(points.value));
+  degreeIncrementLabel.innerHTML = "Point every " + degreeIncrement.toFixed(2) + " degrees";
+}
+
+function setSpiralGapLabel() {
+  var spiralGap = Math.abs(outerR.value - innerR.value) / revolutions.value;
+  spiralGapLabel.innerHTML = "Gap of " + spiralGap.toFixed(2) + " between each revolution of the spiral";
+}
+
+document.getElementById('spiral-button-1').addEventListener('click',function(){
+  console.log('SPIRAL DEBUG // Spiral Button Clicked');
+  // Create JSON object with the action we want to trigger and the current UNIX date
+  var data = {
+    "spiral": "Archimedean Spiral",
+    "innerRadius": innerR.value,
+    "outerRadius": outerR.value,
+    "degrees": degrees.value,
+    "points": points.value,
+    "date": new Date().getTime()
+  }
+  console.log(data);
+  console.log(JSON.stringify(data));
+  // Put the JSON as a string in the hash
+  window.location.hash = JSON.stringify(data);
+  return false;
+});
---
+++ 6Spiral.sketchplugin/Contents/Sketch/plugin.js
@@ -0,0 +1,231 @@
+@import "MochaJSDelegate.js";
+
+function onRun(context) {
+
+  var doc = context.document;
+  var selectedLayers = context.selection;
+  var selectedCount = selectedLayers.count();
+
+  var panelWidth = 400; // original 80
+  var panelHeight = 800; // original 240
+
+  // Create an NSThread dictionary with a specific identifier
+  var threadDictionary = NSThread.mainThread().threadDictionary();
+  //   var identifier = "co.awkward.floatingexample";
+  var identifier = "design.matej.6spiral";
+
+  // If there's already a panel, prevent the plugin from running
+  if (threadDictionary[identifier]) return;
+
+  // Create the panel and set its appearance
+  var panel = NSPanel.alloc().init();
+  panel.setFrame_display(NSMakeRect(0, 0, panelWidth, panelHeight), true);
+  panel.setStyleMask(NSTexturedBackgroundWindowMask | NSTitledWindowMask | NSClosableWindowMask | NSFullSizeContentViewWindowMask);
+  panel.setBackgroundColor(NSColor.whiteColor());
+
+  // Set the panel's title and title bar appearance
+  panel.title = "🌀6Spiral";
+  panel.titlebarAppearsTransparent = true;
+
+  // Center and focus the panel
+  panel.center();
+  panel.makeKeyAndOrderFront(null);
+  panel.setLevel(NSFloatingWindowLevel);
+
+  // Make the plugin's code stick around (since it's a floating panel)
+  COScript.currentCOScript().setShouldKeepAround_(true);
+
+  // Hide the Minimize and Zoom button
+  panel.standardWindowButton(NSWindowMiniaturizeButton).setHidden(true);
+  panel.standardWindowButton(NSWindowZoomButton).setHidden(true);
+
+  // Create the blurred background
+  var vibrancy = NSVisualEffectView.alloc().initWithFrame(NSMakeRect(0, 0, panelWidth, panelHeight));
+  vibrancy.setAppearance(NSAppearance.appearanceNamed(NSAppearanceNameVibrantLight));
+  vibrancy.setBlendingMode(NSVisualEffectBlendingModeBehindWindow);
+
+  // Create the WebView with a request to a Web page in Contents/Resources/
+  var webView = WebView.alloc().initWithFrame(NSMakeRect(0, 0, panelWidth, panelHeight - 44));
+  var request = NSURLRequest.requestWithURL(context.plugin.urlForResourceNamed("webView.html"));
+  webView.mainFrame().loadRequest(request);
+  // Commented out so you can Right Click -> Inspect Element
+  // webView.setDrawsBackground(false);
+
+  // Access the Web page's JavaScript environment
+  var windowObject = webView.windowScriptObject();
+
+  // Create the delegate
+  var delegate = new MochaJSDelegate({
+
+    // Listen to URL changes
+    "webView:didChangeLocationWithinPageForFrame:": (function(webView, webFrame) {
+
+      // Extract the URL hash (without #) by executing JavaScript in the Web page
+      var hash = windowObject.evaluateWebScript("window.location.hash.substring(1)");
+
+      // Parse the hash's JSON content
+      var data = JSON.parse(hash);
+      log(data);
+
+      if (data.hasOwnProperty('action')) {
+        // Launch a Sketch action and focus the main window
+        context.document.actionsController().actionForID(data.action).doPerformAction(null);
+        NSApp.mainWindow().makeKeyAndOrderFront(null);
+      }
+
+      if (data.hasOwnProperty('spiral')) {
+        // Make a SPIRAL.
+        log("SPIRAL DEBUG // PRESSED MAKE SPIRAL BUTTON!!!");
+        log("SPIRAL DEBUG // " + selectedLayers);
+        log("SPIRAL DEBUG // " + selectedCount);
+        if(selectedCount != 0) {
+            var layer = selectedLayers[0];
+            log("SPIRAL DEBUG // " + layer);
+            //check that this layer is a shape
+            if (layer && layer.isKindOfClass(MSShapeGroup)) {
+                layer.select_byExtendingSelection(true, false);
+                addSpiral(layer, data);
+        
+                //group and name new group
+                log("SPIRAL DEBUG // STARTED MAKING GROUP");
+                var groupAction = doc.actionsController().actionForID("MSGroupAction");
+                groupAction.group(nil);
+                gr = layer.parentGroup();
+        
+                if ((layer.name() == 'Line') || (layer.name() == 'Path')) {
+                    gr.setName('ArrowSpiral');
+                } else {
+                    gr.setName(layer.name());
+                }
+                log("SPIRAL DEBUG // FINISHED MAKING GROUP");
+        
+            } else {
+              log("SPIRAL DEBUG // DIDNT MAKE SPIRAL");
+              doc.showMessage('Oops, not a path.😅 ');
+            }
+    
+        }
+      }
+      panel.close();
+    })
+  })
+
+  // Set the delegate on the WebView
+  webView.setFrameLoadDelegate_(delegate.getClassInstance());
+
+  // Add the content views to the panel
+  panel.contentView().addSubview(vibrancy);
+  panel.contentView().addSubview(webView);
+
+  // After creating the panel, store a reference to it
+  threadDictionary[identifier] = panel;
+
+  var closeButton = panel.standardWindowButton(NSWindowCloseButton);
+
+  // Assign a function to the Close button
+  closeButton.setCOSJSTargetFunction(function(sender) {
+    panel.close();
+
+    // Remove the reference to the panel
+    threadDictionary.removeObjectForKey(identifier);
+
+    // Stop the plugin
+    COScript.currentCOScript().setShouldKeepAround_(false);
+  });
+}
+
+function addSpiral(layer, data) {
+
+  var innerR = parseInt(data.innerRadius);
+  var outerR = parseInt(data.outerRadius);
+  var degrees = parseInt(data.degrees);
+  var points = parseInt(data.points);
+
+  // final outer R is 93.2 now (50 + 54 * 0.8)
+  // (93.2 - 50) / 54
+  var numOfPoints = 55;
+  var pointDistanceIncrement = (outerR - innerR)/(numOfPoints-1);
+
+  log("SPIRAL DEBUG // Making Spiral")
+  log("SPIRAL DEBUG // " + pointDistanceIncrement);
+  log("SPIRAL DEBUG // " + data.innerRadius);
+  log("SPIRAL DEBUG // " + data.outerRadius);
+  var spiralPath = NSBezierPath.bezierPath();
+  spiralPath.moveToPoint(NSMakePoint(0, 0));
+  for (var i = 0; i < numOfPoints; i++) {
+
+    var pointLength = innerR + i * 0.8; // default was 50.
+    log('SPIRAL DEBUG // pointLength: ' + pointLength);
+
+    var zigZag = false;
+    var lengthChange = 0;
+    if (zigZag) {
+      var lengthChange = pointLength / 10;
+      if (i % 2 == 1) {
+        lengthChange = -pointLength / 10;
+      }
+    }
+    // var centerToPointLength = Math.sqrt(pointX*pointX+pointY*pointY); // length is defined.
+    // var centerToPointNormalVectorX = pointX / centerToPointLength;
+    // var centerToPointNormalVectorY = pointY / centerToPointLength;
+
+    var pointAngle = i * 10;
+    pointLength = pointLength + lengthChange;
+    var pointX = pointLength * Math.cos(pointAngle * (Math.PI / 180));
+    var pointY = pointLength * Math.sin(pointAngle * (Math.PI / 180));
+    log('SPIRAL DEBUG // Point X: ' + pointX + ' Point Y: ' + pointY);
+    log('SPIRAL DEBUG // pointLength: ' + pointLength);
+
+    spiralPath.lineToPoint(NSMakePoint(pointX, pointY));
+  }
+  //spiralPath.closePath();
+  spiralPath = MSPath.pathWithBezierPath(spiralPath);
+  var spiralShape = MSShapeGroup.shapeWithBezierPath(spiralPath);
+
+
+  //scale to lineweight
+  //TBD: user input
+  var lineThickness = layer.style().borders().objectAtIndex(0).thickness();
+  var scale = 1 + (lineThickness / 5);
+  // spiralShape.frame().setWidth(Math.floor(spiralShape.frame().width() * scale));
+  // spiralShape.frame().setHeight(Math.floor(spiralShape.frame().height() * scale));
+
+  //color same as line
+  // var fill = spiralShape.style().addStylePartOfType(0);
+  // fill.setFillType(0);
+  // var fills = spiralShape.style().enabledFills();
+  // fills.lastObject().setColor(layer.style().borders().firstObject().color());
+
+  var border = spiralShape.style().addStylePartOfType(1);
+  border.color = MSImmutableColor.colorWithSVGString("#979797");
+
+  //add to layer
+  spiralShape.setName('SPIRALshape');
+  var gr = layer.parentGroup();
+  gr.addLayers([spiralShape]);
+  spiralShape.select_byExtendingSelection(true, true);
+
+  log("SPIRAL DEBUG // Completed Making Spiral")
+}
+
+var onSelectionChanged = function(context) {
+  var threadDictionary = NSThread.mainThread().threadDictionary();
+  var identifier = "design.matej.6spiral";
+
+  // Check if there's a panel opened or not
+  if (threadDictionary[identifier]) {
+    var panel = threadDictionary[identifier];
+
+    // Access the panel from the reference and the WebView
+    var webView = panel.contentView().subviews()[1];
+    var windowObject = webView.windowScriptObject();
+
+    // Get the current selection and update the CSS preview accordingly
+    var selection = context.actionContext.document.selectedLayers().layers();
+    if (selection.length == 1) {
+      windowObject.evaluateWebScript("updatePreview('" + selection[0].CSSAttributes().join(" ") + "')");
+    } else {
+      windowObject.evaluateWebScript("updatePreview(' ')");
+    }
+  }
+};
